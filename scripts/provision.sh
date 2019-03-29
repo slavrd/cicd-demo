bdcc9b9721d2f1d2ddcbacd581bf6e963107b546
Slav Donchev <slav.donchev@gmail.com>
2019-03-29 11:18:56 +0200

    update provision script to install inspec

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -9,3 +9,9 @@ which nginx || {
 # deploy website
 sudo rm /var/www/html/*
 sudo cp index.html /var/www/html/.
+
+# install inspec 3.7.11
+which inspec || {
+    wget -q -P /tmp https://packages.chef.io/files/stable/inspec/3.7.11/ubuntu/16.04/inspec_3.7.11-1_amd64.deb
+    sudo dpkg -i /tmp/inspec_3.7.11-1_amd64.deb
+}
