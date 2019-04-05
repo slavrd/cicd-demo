4133a92c1505777760403f44b711a26ed960a9df
Slav Donchev <slav.donchev@gmail.com>
2019-04-05 13:29:36 +0300

    update provision script

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# install nginx
-which nginx || {
-    sudo apt-get update
-    sudo apt-get install -y nginx
-}
-
-# deploy website
-sudo rm /var/www/html/*
-sudo cp index.html /var/www/html/.
-
 # install inspec 3.7.11
 which inspec || {
     wget -q -P /tmp https://packages.chef.io/files/stable/inspec/3.7.11/ubuntu/16.04/inspec_3.7.11-1_amd64.deb
